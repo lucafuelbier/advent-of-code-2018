b41d2925dbeec2993b821c79ee49fb037fae0131
Luca Fülbier <luca.fuelbier94@gmail.com>
2018-12-01 11:00:53 +0100

    Add solution 1-1

---
+++ day1_1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solution = File.ReadLines("Input.txt")
+                               .Select(fc => Int32.Parse(fc))
+                               .Aggregate((fc1, fc2) => fc1 + fc2);
+            
+            Console.WriteLine(solution);
+        }
+    }
+}
