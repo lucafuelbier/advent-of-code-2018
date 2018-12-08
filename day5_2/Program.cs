d46307e1d3a091848039f0f25f2ac0dbc925546e
Luca Fülbier <luca.fuelbier94@gmail.com>
2018-12-08 19:07:56 +0100

    Add solution 5-2

---
+++ day5_2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+namespace day5_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var polymer = File.ReadAllText("Input.txt");
+            var shortest = polymer.Length;
+
+            foreach (char c in "abcdefghijklmnopqrstuvwxyz")
+            {
+                var replacementPattern = $"[{c}{Char.ToUpper(c)}]";
+
+                var collapsedPolymer = Regex.Replace(polymer, replacementPattern, "");
+
+                var stack = new Stack<char>();
+
+                foreach (char p in collapsedPolymer)
+                {
+                    if (stack.Count == 0)
+                    {
+                        stack.Push(p);
+                    }
+                    else if (IsOpposite(stack.Peek(), p))
+                    {
+                        stack.Pop();
+                    }
+                    else
+                    {
+                        stack.Push(p);
+                    }
+                }
+
+                if (stack.Count < shortest)
+                {
+                    shortest = stack.Count;
+                }
+            }
+
+            Console.WriteLine(shortest);
+        }
+
+        static bool IsOpposite(char x, char y)
+        {
+            return Math.Abs(x.CompareTo(y)) == 32;
+        }
+    }
+}
