e4c4a4a1c1ca598f4cbb17da1f83e2d5eee65afa
Luca Fülbier <luca.fuelbier94@gmail.com>
2018-12-08 18:13:38 +0100

    Add solution 5-1

---
+++ day5_1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace day5_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var polymer = File.ReadAllText("Input.txt");
+
+            var stack = new Stack<char>();
+
+            foreach (char c in polymer)
+            {
+                if (stack.Count == 0)
+                {
+                    stack.Push(c);
+                }
+                else if (IsOpposite(stack.Peek(), c))
+                {
+                    stack.Pop();
+                }
+                else
+                {
+                    stack.Push(c);
+                }
+            }
+
+            var solution = stack.Count;
+
+            Console.WriteLine(solution);
+        }
+
+        static bool IsOpposite(char x, char y)
+        {
+            return Math.Abs(x.CompareTo(y)) == 32;
+        }
+    }
+}
